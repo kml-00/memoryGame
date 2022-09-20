b9b58c42be3e9dc1c0c6cd5b87ce870241a905e5
kml <kamillo57@wp.pl>
2022-09-20 20:38:45 +0100

    control game script has been created

--- script.js
+++ script.js
@@ -1,11 +1,57 @@
 const cards = document.querySelectorAll('.game-card');
 
-let firstCard,secondCard;
+let cardFlip=[];
 
 function flipCard(event)
 {
     event.target.parentElement.classList.add("flip");
+    cardFlip.push(event.target.parentElement);
+    if(cardFlip.length === 2)
+    {
+       
+        checkCardsMatch(cardFlip)?  setTimeout(deleteCards,1000,cardFlip):setTimeout(unflipCards,1000,cardFlip);
+    }
+
+    console.log(cardFlip);
 } 
 
+function checkCardsMatch(cards) {
+  let match = cards[0].dataset.card === cards[1].dataset.card;
+  return match;
+}
+
+
+function deleteCards(cards) {
+   
+        cards.forEach(element => {
+            getElement= document.getElementById(element.id);
+            
+            while (getElement.firstChild) {
+                element.removeChild(element.firstChild);
+              }
+            }
+        )
+        clearShadow(cards);
+        resetCardsArr()
+        
+}
+
+function unflipCards(cards) {
+    cards.forEach(element => {
+        element.classList.remove('flip');
+    })
+    resetCardsArr()
+}
+
+function resetCardsArr() {
+    cardFlip=[];
+}
+function clearShadow(cards) {
+    cards.forEach(element => {
+        getElement= document.getElementById(element.id);
+        getElement.style.boxShadow="none";
+})
+    
+}
 
 cards.forEach(card => card.addEventListener("click", flipCard));
